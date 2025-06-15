424c5f44a9972099ca08e8200cdda9bdcb1d3f78
Husinat <abiodunhusinat@gmail.com>
2025-06-15 19:01:58 +0100

    tried the second way of doing the counter with js using a single event listener and currenttarget.classlist

--- counter.js
+++ counter.js
@@ -1,25 +1,54 @@
-let count = 0;  
-
-let increaseBtn = document.getElementById('increaseBtn');
-let resetBtn = document.getElementById('resetBtn');
-let decreaseBtn = document.getElementById('decreaseBtn');
-const counter = document.getElementById('counter')
+// JS for the first approach to the counter app
 
 
-increaseBtn.addEventListener('click', function(){
-  count++
-  counter.innerHTML = count;
-  counter.style.color = 'green';
+// let count = 0;
+// let increaseBtn = document.getElementById('increase');
+// let resetBtn = document.getElementById('reset');
+// let decreaseBtn = document.getElementById('decrease');
+// let counter = document.getElementById('counter');
+
+
+// increaseBtn.addEventListener('click', function(){
+//   count++
+//   counter.innerHTML = count;
+//   counter.style.color = 'green';
+// });
+
+// resetBtn.addEventListener('click', function(){
+//   count = 0;
+//     counter.innerHTML = count;
+//     counter.style.color = '#222';
+// })
+
+// decreaseBtn.addEventListener('click', function(){
+//      count--;
+//     counter.innerHTML = count;
+//     counter.style.color = 'red';
+// })
+
+
+
+// Second approach to the counter app using a single event listener
+let count = 0;
+
+const counter = document.getElementById('counter');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function(e) {
+    const styles = e.currentTarget.classList;
+
+    if (styles.contains('increase')) {
+      count++;
+      counter.style.color = 'green';
+    } else if (styles.contains('reset')) {
+      count = 0;
+      counter.style.color = '#222';
+    } else if (styles.contains('decrease')) {
+      count--;
+      counter.style.color = 'red';
+    }
+    
+counter.textContent = count;
+  });
 });
-
-resetBtn.addEventListener('click', function(){
-  count = 0;
-    counter.innerHTML = count;
-    counter.style.color = '#222';
-})
-
-decreaseBtn.addEventListener('click', function(){
-     count--;
-    counter.innerHTML = count;
-    counter.style.color = 'red';
-})
